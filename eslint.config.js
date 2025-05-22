dc1fd9a51e706793d43e8c0dfb32c17cc758163f
pvntheraxxx <pvntheraxxx@gmail.com>
2025-05-22 07:25:15 +0300

    project setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,28 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier", "import"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
-]);
+};
